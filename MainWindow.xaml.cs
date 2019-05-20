2933017b7fd009b1c3e4e1c5b5e7240cffe1b137
Artur <amarszalek97@wp.pl>
2019-05-21 01:28:01 +0200

    Added new views - not completed!

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,83 @@ namespace WalletWPF
         public MainWindow()
         {
             InitializeComponent();
+
+            Main.Content = new MainView();
+            Consumo consumo = new Consumo();
+            DataContext = new ConsumoViewModel(consumo);
+        }
+
+        private void ButtonFechar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void GridBarraTitulo_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Raports();
+        }
+
+        private void Transactions_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Transactions();
+        }
+
+        private void Orders_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Orders();
+        }
+
+        private void Commitment_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Commitment();
+        }
+
+        private void Payments_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Payments();
+        }
+
+        private void Currencys_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Currency();
+        }
+
+        private void MainPage_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new MainView();
+        }
+    }
+
+    internal class ConsumoViewModel
+    {
+        public List<Consumo> Consumo { get; private set; }
+
+        public ConsumoViewModel(Consumo consumo)
+        {
+            Consumo = new List<Consumo>();
+            Consumo.Add(consumo);
+        }
+    }
+
+    internal class Consumo
+    {
+        public string Titulo { get; private set; }
+        public int Porcentagem { get; private set; }
+
+        public Consumo()
+        {
+            Titulo = "";
+            Porcentagem = CalcularPorcentagem();
+        }
+
+        private int CalcularPorcentagem()
+        {
+            return 47; //Calculo da porcentagem de consumo
         }
     }
 }
