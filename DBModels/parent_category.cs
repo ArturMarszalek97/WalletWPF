8261614c71e914ea78545f493f8517f6e160045b
Artur <amarszalek97@wp.pl>
2019-06-03 22:52:26 +0200

    parent_category was created and implemented

---
+++ DBModels/parent_category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletWPF.DBModels
+{
+    public class parent_category
+    {
+        public string name { get; set; }
+        public int? id_parent_category { get; set; }
+        public int? color { get; set; }
+        public int? id_wallet { get; set; }
+    }
+}
