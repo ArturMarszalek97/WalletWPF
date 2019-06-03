14d40d378c733312727e06b55b2d08e9359ddb7a
Artur <amarszalek97@wp.pl>
2019-06-03 22:58:09 +0200

    account was created and implemented

---
+++ DBModels/account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletWPF.DBModels
+{
+    public class account
+    {
+        public string wallet_name { get; set; }
+        public int? id_wallet { get; set; }
+        public int? balance { get; set; }
+    }
+}
