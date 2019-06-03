5252a621d72b6de0fbde9c7894d52b8bb8302367
Artur <amarszalek97@wp.pl>
2019-06-03 22:49:46 +0200

    income was created and implemented

---
+++ DBModels/income.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletWPF.DBModels
+{
+    public class income
+    {
+        public string name { get; set; }
+        public string comment { get; set; }
+        public int? id_income { get; set; }
+        public int? id_subcategory { get; set; }
+        public float amount { get; set; }
+        public DateTime? date { get; set; }
+    }
+}
