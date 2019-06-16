a48ed1d85d71dd4e2fb26efb143e4a6b933fcf56
Cezary Chojnowski <chojnowski.czarek@gmail.com>
2019-06-16 14:40:13 +0200

    List Payment method from database. Iterating PK

--- AddTransaction.xaml.cs
+++ AddTransaction.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,27 @@ namespace WalletWPF
     /// </summary>
     public partial class AddTransaction : Window
     {
+
         public AddTransaction()
         {
+            ListPaymentMethod();
             InitializeComponent();
+        }
+
+        public List<PaymentMethod> PaymentMethods { get; set; }
+        private void ListPaymentMethod()
+        {
+            walletdbEntities1 dc = new walletdbEntities1();
+            var item = dc.PaymentMethod.ToList();
+            PaymentMethods = item;
+            DataContext = PaymentMethods;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = combobox1.SelectedItem as PaymentMethod;
+            MessageBox.Show(item.name.ToString());
+
         }
     }
 }
